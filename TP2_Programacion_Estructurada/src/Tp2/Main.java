d305a0e54e6d66b49192cb33e4ab76f15ad34cf5
Isaias Javier Ovelar <Isaiasjovelar@gmail.com>
2025-08-17 06:33:13 -0300

    Hasta el punto 6, a completar

--- TP2_Programacion_Estructurada/src/Tp2/Main.java
+++ TP2_Programacion_Estructurada/src/Tp2/Main.java
@@ -87,18 +87,50 @@ public class Main {
     {
         System.out.println("### Punto 1 ###");
         System.out.println("Ingrese el precio del producto: ");
+        float precioBase=scan.nextFloat();
         String opcion="x";
         do{
-            try{
-                System.out.println("Ingrese la clase de producto. A B C");
-                opcion=scan.nextLine();
-            }
-            catch(Exception e)
+            System.out.println("Ingrese la clase de producto. A B C");
+            opcion=scan.nextLine();
+            if(!opcion.toUpperCase().equals("A") && !opcion.toUpperCase().equals("B") && !opcion.toUpperCase().equals("C"))
             {
-                System.out.println("Opcion invalida, solo A B C");
+                System.out.println("Opcion invalida");
             }
         }
-        while(!opcion.equals("a") && !opcion.equals("b") && !opcion.equals("c"));
+        while(!opcion.toUpperCase().equals("A") && !opcion.toUpperCase().equals("B") && !opcion.toUpperCase().equals("C"));
+        float precioFinal;
+        int porcentajeDescuento;
+        if(opcion.toUpperCase().equals("A"))
+        {
+            precioFinal=precioBase*(float)0.90;
+            porcentajeDescuento=10;
+        }
+        else if(opcion.toUpperCase().equals("b"))
+        {
+            precioFinal=precioBase*(float)0.85;
+            porcentajeDescuento=15;
+        }
+        else
+        {
+            precioFinal=precioBase*(float)0.8;
+            porcentajeDescuento=20;
+        }
+        System.out.println("Precio Base: "+precioBase+"\n Porcentaje descuento: "+porcentajeDescuento+" \n Precio Final: "+precioFinal);
+    }
+    
+    public static void punto5(Scanner scan)
+    {
+        int numero;
+        int suma=0;
+        do{
+            System.out.println("Ingrese un numero (0 para salir): ");
+            numero=scan.nextInt();
+            if(numero%2==0)
+            {
+                suma+=numero;
+            }
+        }while(numero!=0);
+        System.out.println("Suma: "+suma);
     }
     /**
      * @param args the command line arguments
@@ -108,7 +140,8 @@ public class Main {
         //punto1(scan);
         //punto2(scan);
         //punto3(scan);
-        punto4(scan);
+        //punto4(scan);
+        punto5(scan);
         
         scan.close();
     }
